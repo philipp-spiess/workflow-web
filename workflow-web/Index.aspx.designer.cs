708489f516ab35ef544038cebb33c1dafb98c586
unknown <Nicolas Dular@WIN-IERH36LOR3F.(none)>
2012-03-22 14:01:02 +0100

    hh

---
+++ workflow-web/Index.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <automatisch generiert>
+//     Der Code wurde von einem Tool generiert.
+//
+//     Änderungen an der Datei führen möglicherweise zu falschem Verhalten, und sie gehen verloren, wenn
+//     der Code erneut generiert wird.
+// </automatisch generiert>
+//------------------------------------------------------------------------------
+
+namespace workflow_web {
+    
+    
+    public partial class _Default {
+        
+        /// <summary>
+        /// ProgrammListe-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ProgrammListe;
+        
+        /// <summary>
+        /// ArbeitsauftraegeListe-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ArbeitsauftraegeListe;
+        
+        /// <summary>
+        /// StartProgram-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button StartProgram;
+        
+        /// <summary>
+        /// Weiterfuehren-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Um dies zu ändern, verschieben Sie die Felddeklaration aus der Designerdatei in eine Code-Behind-Datei.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Weiterfuehren;
+    }
+}
